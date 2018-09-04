dcfab468bb10f21879d564ac3d2b81299c5806ac
Brenden Seidel <brenseid@gmail.com>
2018-09-04 10:30:02 -0400

    refactor app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
 const weather = require('./weather');
-
-const location = process.argv.slice(2).reverse().join("/");
-// console.log(location);
-weather.get(location);
+//Join multiple values passed as arguments and replace all spaces with underscores
+const query = process.argv.slice(2).join("_").replace(' ', '_');
+//query: 90201
+//query: Cleveland_OH
+//query: London_England
+weather.get(query);
--- weather.js
+++ weather.js
@@ -1,19 +1,59 @@
-// Require https module
 const http = require('http');
+const https = require('https');
+const api = require('./api.json');
 
-function getWeather(location) {
-  const request = http.get(`http://api.wunderground.com/api/<key>/conditions/q/${location}.json`, response =>{
-    let body = "";
-    response.on('data', data => {
-      body += data.toString();
-    });
-    response.on('end', () => {
-      body = JSON.parse(body);
-      const currentTemp = body.current_observation.temp_f;
-      locationString = location.split('/').reverse().join(', ');
-      console.log(`The current temperature in ${locationString} is ${currentTemp}°F.`);
-    });
-  });
+// Print out temp details
+function printWeather(weather) {
+    const message = `Current temperature in ${weather.location.city} is ${weather.current_observation.temp_f}F`;
+    console.log(message);
 }
 
-module.exports.get = getWeather;
+// Print out error message
+function printError(error) {
+    console.error(error.message);
+}
+
+function get(query) {
+    // Take out underscores for readability
+    const readableQuery = query.replace('_', ' ');
+    try {
+        const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                // Read the data
+                response.on('data', chunk => {
+                    body += chunk;
+                });
+                response.on('end', () => {
+                    try {
+                        // Parse the data
+                        const weather = JSON.parse(body);
+                        // Check if the location was found before printing
+                        if (weather.location) {
+                            // Print the data
+                            printWeather(weather);
+                        } else {
+                            const queryError = new Error(`The location "${readableQuery}" was not found.`);
+                            printError(queryError);
+                        }
+                    } catch (error) {
+                        // Parse Error
+                        printError(error);
+                    }
+                });
+            } else {
+                // Status Code Error
+                const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+                printError(statusCodeError);
+            }
+
+        });
+
+        request.on("error", printError);
+    } catch (error) {
+        //Malformed URL Error
+        printError(error);
+    }
+}
+
+module.exports.get = get;
